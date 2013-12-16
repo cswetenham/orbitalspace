6bc3a393cc782a989e3ccf14d3fbb6d7b3a9a3c2
Chris Swetenham <cswetenham@gmail.com>
2013-12-15 19:10:34 -0500

    Add the utility functions for shader/program loading from the tutorial I'm using.

--- src/orApp.cpp
+++ src/orApp.cpp
@@ -274,6 +274,93 @@ orApp::FrameBuffer orApp::makeFrameBuffer(int width, int height)
   return result;
 }
 
+// TODO cleanup
+char* file_contents(const char* filename, GLint* length)
+{
+    FILE* f = fopen(filename, "r");
+    char* buffer;
+
+    if (!f) {
+        fprintf(stderr, "Unable to open %s for reading\n", filename);
+        return NULL;
+    }
+
+    fseek(f, 0, SEEK_END);
+    *length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    buffer = (char*)malloc(*length+1);
+    *length = fread(buffer, 1, *length, f);
+    fclose(f);
+    buffer[*length] = '\0';
+
+    return buffer;
+}
+
+// TODO cleanup
+static void show_info_log(
+    GLuint object,
+    PFNGLGETSHADERIVPROC glGet__iv,
+    PFNGLGETSHADERINFOLOGPROC glGet__InfoLog
+)
+{
+    GLint log_length;
+    char* log;
+
+    glGet__iv(object, GL_INFO_LOG_LENGTH, &log_length);
+    log = (char*)malloc(log_length);
+    glGet__InfoLog(object, log_length, NULL, log);
+    fprintf(stderr, "%s", log);
+    free(log);
+}
+
+// TODO cleanup
+static GLuint make_shader(GLenum type, const char* filename)
+{
+    GLint length;
+    GLchar* source = file_contents(filename, &length);
+    GLuint shader;
+    GLint shader_ok;
+
+    if (!source)
+        return 0;
+
+    shader = glCreateShader(type);
+    glShaderSource(shader, 1, (const GLchar**)&source, &length);
+    free(source);
+    glCompileShader(shader);
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &shader_ok);
+    if (!shader_ok) {
+        fprintf(stderr, "Failed to compile %s:\n", filename);
+        show_info_log(shader, glGetShaderiv, glGetShaderInfoLog);
+        glDeleteShader(shader);
+        return 0;
+    }
+    return shader;
+}
+
+// TODO cleanup
+static GLuint make_program(GLuint vertex_shader, GLuint fragment_shader)
+{
+    GLint program_ok;
+
+    GLuint program = glCreateProgram();
+
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    glGetProgramiv(program, GL_LINK_STATUS, &program_ok);
+    if (!program_ok) {
+        fprintf(stderr, "Failed to link shader program:\n");
+        show_info_log(program, glGetProgramiv, glGetProgramInfoLog);
+        glDeleteProgram(program);
+        return 0;
+    }
+    return program;
+}
+
 void orApp::InitRender()
 {
   // TODO z-ordering seems bad on text labels (Moon label appears in front of everything else - not sure if this is what I want, would be good to have the option)
